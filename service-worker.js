d1eca1ff7957f1b630e40f1a4d24b14c9c0ff7c2
KevinPoullieS <poulliekev98@schueler.biz-worms.de>
2026-04-21 08:12:06 +0200

    Refactor service worker install event handling
    Removed old service worker event listeners and added skipWaiting.

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,3 @@
 self.addEventListener("install", e=>{
- e.waitUntil(caches.open("v1").then(c=>c.addAll(["/"])))
-});
-
-self.addEventListener("fetch", e=>{
- e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))
+ self.skipWaiting();
 });
